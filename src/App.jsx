ea6d4fd473ccf30611efc552575236bc9675e68a
Diana Whealan <dianawhealan@Dianas-MacBook-Air.local>
2024-10-01 18:29:09 -0500

    Fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 
 import Banner from './components/Banner'
 import CourseList from './components/CourseList';
+import { useJsonQuery } from './utilities/fetch';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -36,15 +39,29 @@ const schedule = {
   }
 };
 
-const App = () => {
-  
+const Main = () => {
+  const [data, isLoading, error] = useJsonQuery("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php");
 
+  if (error) return <h1>Error loading course data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading course data...</h1>;
+  if (!data) return <h1>No course data found</h1>;
+    
   return (
     <div className="App">
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
+      <Banner title={data.title} />
+      <CourseList courses={data.courses} />
     </div>
   );
 };
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <div className="container">
+      <Main />
+    </div>
+  </QueryClientProvider>
+);
 
 export default App;
